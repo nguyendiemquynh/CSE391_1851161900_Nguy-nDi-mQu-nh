42fe3fae84ce7e58d9c512a4153ea9f3dd7c7dd7
nguyendiemquynh <52966421+nguyendiemquynh@users.noreply.github.com>
2020-07-31 09:50:59 +0700

    Delete script.js

--- baith3/script.js
+++
@@ -1,7 +0,0 @@
-$("#sltScaletime").on(e 'change', t function(){
-    var scaleNum =$(this).val();
-    $(".scale-time-dotail").html('');
-    for(let j=8;i<scaleNum;j++){
-        $(".scale-time-detail").append('thời đoạn '+j+' : '+' <input type= "text"><br>')
-    }
-})
